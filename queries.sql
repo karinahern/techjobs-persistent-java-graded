8af6eb968ba62246173a2919f0e461959d60c5d5
Karina Hernandez <kmhernandez000@gmail.com>
2022-11-13 22:42:31 -0600

    Corrected Task 4 Query.

--- queries.sql
+++ queries.sql
@@ -10,7 +10,7 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 DROP TABLE job;
 
 -- Part 4: Test it with SQL
-SELECT FROM skill
-LEFT INNER JOIN job_skills ON skill.id = job_skills.skills_id
-WHERE job_skills.jobs.id IS NOT NULL
+SELECT * FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
